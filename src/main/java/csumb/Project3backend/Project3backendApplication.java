6734bd9eeead347c15b72876ced9a17cf690cd62
yoo-loc <mgarban@csumb.edu>
2024-10-29 12:31:11 -0700

    Initial commit for Project3backend

---
+++ src/main/java/csumb/Project3backend/Project3backendApplication.java
@@ -0,0 +1,13 @@
+package csumb.Project3backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3backendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project3backendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/csumb/Project3backend/Project3backendApplicationTests.java
@@ -0,0 +1,13 @@
+package csumb.Project3backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project3backendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
